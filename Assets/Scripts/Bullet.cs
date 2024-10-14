249c87e6e62609bbd3da4b030f16ef319fe224b5
Frans Erhart <franserhart12@gmail.com>
2024-10-14 13:20:21 +0200

    Bullet script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -14,8 +14,10 @@ public class Bullet : MonoBehaviour
         //Slaat de Rigidbody op in een variabele.
         m_rb = GetComponent<Rigidbody2D>();
 
+
         //Schiet de kogel af op een bepaalde snelheid.
         m_rb.velocity = transform.up * m_bulletSpeed;
+
 
         //Als de kogel niets raakt dan destroyed het zichzelf na 3 seconden.
         Destroy(gameObject, 3f);
@@ -24,9 +26,6 @@ public class Bullet : MonoBehaviour
     //Als de gameObject iets raakt gaat ie stuk.
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Voordat de bullet gedestroyed word spawned er eerst een particle effect in. (m_explosionPrefab)
-/*        Instantiate(m_explosionPrefab, transform.position, Quaternion.identity);*/
-        Destroy(this.gameObject);
+        Destroy(gameObject);
     }
-
 }
