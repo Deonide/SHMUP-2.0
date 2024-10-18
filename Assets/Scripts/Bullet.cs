af2b6ce95372cbb11c5adb750204875fd76e7b3a
Frans Erhart <franserhart12@gmail.com>
2024-10-18 13:45:39 +0200

    Bullet + GameManager changes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,25 +7,47 @@ public class Bullet : MonoBehaviour
     [SerializeField]
     private int m_bulletSpeed = 10;
 
+
+
     private Rigidbody2D m_rb;
+    private PlayerMovement m_player;
 
     void Start()
     {
         //Slaat de Rigidbody op in een variabele.
         m_rb = GetComponent<Rigidbody2D>();
 
-
         //Schiet de kogel af op een bepaalde snelheid.
         m_rb.velocity = transform.up * m_bulletSpeed;
 
-
         //Als de kogel niets raakt dan destroyed het zichzelf na 3 seconden.
         Destroy(gameObject, 3f);
+
+        m_player = FindObjectOfType<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (m_player.m_bulletPowered != true)
+        {
+            m_player.m_bulletDamage = 1;
+        }
+        else if (m_player.m_bulletPowered == true)
+        {
+            m_player.m_bulletDamage = 2;
+        }
+        else if (m_player.m_bulletUltra != true)
+        {
+            m_player.m_bulletDamage = 3;
+        }
     }
 
     //Als de gameObject iets raakt gaat ie stuk.
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,45 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.SocialPlatforms.Impl;
+
 
 public class GameManager : Singleton<GameManager>
 {
-
-    [SerializeField]
-    private GameObject m_pauseScreen;
-
-
-    private PlayerControls m_playerControls;
-
     public int m_currentWave;
 
     protected override void Awake()
     {
         Time.timeScale = 1.0f;
-        m_playerControls = new PlayerControls();
-        m_playerControls.PlayerMovement.Pause.Enable();
-        m_playerControls.PlayerMovement.Pause.performed += OnPause;
 
-        m_pauseScreen.SetActive(false);
         base.Awake();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
-    private void OnPause(InputAction.CallbackContext _context)
-    {
-        Time.timeScale = 0;
-        m_pauseScreen.SetActive(true);
     }
 }
