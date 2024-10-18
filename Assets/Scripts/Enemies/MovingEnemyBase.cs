a7d08579f1593b89a040456e5c4219c770d42eb9
Frans Erhart <franserhart12@gmail.com>
2024-10-18 13:44:20 +0200

    MovingEnemyBase

--- Assets/Scripts/Enemies/MovingEnemyBase.cs
+++ Assets/Scripts/Enemies/MovingEnemyBase.cs
@@ -10,24 +10,18 @@ public class MovingEnemyBase : EnemyBase
 
     protected override void Start()
     {
-        m_enemySpeed = 1 + Mathf.Round((GameManager.Instance.m_currentWave / 4));
+        m_enemySpeed = 1 + Mathf.Round((GameManager.Instance.m_currentWave / 8));
         base.Start();
     }
 
     protected override void Update()
     {
         base.Update();
-        float enemyPositionY = -m_enemySpeed * Time.deltaTime / 10;
 
         if (m_Health > 0 && m_enemyState == EnemyState.Active)
         {
             m_timer += Time.deltaTime;
             transform.position = Vector2.Lerp(transform.position, new Vector2(Mathf.PingPong(m_timer * m_enemySpeed, m_screenSpace.x), m_enemyPosition.y), Time.deltaTime * m_enemySpeed);
-            
-            if(m_enemyMoves == 2)
-            {
-                transform.Translate(0, enemyPositionY, 0);
-            }
         }
     }
 }
